7b2aa3e4bdb991bbbf792273ff5afa86d39845ce
Eduardo Flores <edflor46@gmail.com>
2021-07-11 19:21:28 -0500

    Tipos de datos numericos

---
+++ src/DataTypes.java
@@ -0,0 +1,15 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        //Int
+        int n = 1234567890;
+
+        //Long
+        long nl = 1234567190123L;
+
+        // Double
+        double nD = 123.456123456;
+
+        // Float
+        float nF = 123.456F;
+    }
+}
